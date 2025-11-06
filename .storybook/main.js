72d3315297bc693c1aa6735590a1ae3d2dcf840c
Iurii Dorofeev <iurii.dorofeev@1win.pro>
2025-11-06 11:19:10 +0100

    ci

--- .storybook/main.js
+++ .storybook/main.js
@@ -78,6 +78,34 @@ module.exports = {
       maxAssetSize: 4000000,
     };
 
+    // Fix chunk loading issues in Storybook 7
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          // Vendor chunk for node_modules
+          vendor: {
+            name: 'vendor',
+            chunks: 'all',
+            test: /node_modules/,
+            priority: 20,
+          },
+          // Common chunk for shared code
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            priority: 10,
+            reuseExistingChunk: true,
+            enforce: true,
+          },
+        },
+      },
+    };
+
     // Return the altered config
     return config;
   },
